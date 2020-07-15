b34e351beb9e632b37df68cec3711a86e490deb2
Andreas Gohr <andi@splitbrain.org>
2020-07-15 12:59:05 +0200

    initial wizard generated checkin

---
+++ action.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * DokuWiki Plugin tos (Action Component)
+ *
+ * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
+ * @author  Andreas Gohr <dokuwiki@cosmocode.de>
+ */
+
+// must be run within Dokuwiki
+if (!defined('DOKU_INC')) {
+    die();
+}
+
+class action_plugin_tos extends DokuWiki_Action_Plugin
+{
+
+    /**
+     * Registers a callback function for a given event
+     *
+     * @param Doku_Event_Handler $controller DokuWiki's event controller object
+     *
+     * @return void
+     */
+    public function register(Doku_Event_Handler $controller)
+    {
+        $controller->register_hook('DOKUWIKI_STARTED', 'FIXME', $this, 'handle_dokuwiki_started');
+   
+    }
+
+    /**
+     * [Custom event handler which performs action]
+     *
+     * Called for event:
+     *
+     * @param Doku_Event $event  event object by reference
+     * @param mixed      $param  [the parameters passed as fifth argument to register_hook() when this
+     *                           handler was registered]
+     *
+     * @return void
+     */
+    public function handle_dokuwiki_started(Doku_Event $event, $param)
+    {
+    }
+
+}
+
---
+++ conf/default.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Default settings for the tos plugin
+ *
+ * @author Andreas Gohr <dokuwiki@cosmocode.de>
+ */
+
+//$conf['fixme']    = 'FIXME';
+
